6836a5c5f3bf2e22390a34698eb795e87cb58b23
Rohit Kumar <rohit@Rohits-MacBook-Air.local>
2026-03-31 14:26:42 +0530

    cors issue

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://fin-pilot-six.vercel.app",
+  origin: true,
   credentials: true
 }));
 app.use("/api/auth" , authRoutes);
